a1bce4d38b2b8b0cacf911a242cbbee386e4faf8
hendri2212 <cowok_cool320@yahoo.co.id>
2025-01-22 15:56:26 +0800

    fix theme color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffc107" />
+        <meta name="theme-color" content="#ffc107"> <!-- Ganti dengan warna yang diinginkan -->
+        <!-- <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffc107" /> -->
         
         <title>Apik - @yield('title')</title>
         <link rel="icon" href="favicon.ico" type="image/x-icon">
