3353d4df55b78165280c723f0e3e8c6990fcb770
hendri2212 <cowok_cool320@yahoo.co.id>
2025-06-06 12:55:53 +0800

    add member store

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -12,7 +12,7 @@ class MemberController extends Controller {
     }
 
     public function create() {
-        return view('members.create');
+        return view('members.add');
     }
 
     /**
@@ -20,7 +20,27 @@ class MemberController extends Controller {
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name'        => 'required|string|max:255',
+            'username'    => 'required|string|max:255|unique:users,username',
+            'password'    => 'required|string',
+            'telegram_id' => 'required|string|max:20',
+            'expired'     => 'required|date',
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->username = $request->username;
+        $user->password = $request->password;
+        $user->telegram_id = $request->telegram_id;
+        $user->expired = $request->expired;
+        $user->uuid = Str::random(16);
+
+        $user->save();
+
+        return redirect()
+            ->route('members.index')
+            ->with('success', 'Member berhasil ditambahkan.');
     }
 
     /**
